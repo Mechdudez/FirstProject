e027389c3dc60d48f1497438156b9ceb8f4c2082
Ryan Wiest <james.wiest@mykenzie.snhu.edu>
2022-06-19 22:04:28 -0700

    Push

--- Application/src/main/java/Restaurants/randomizer/Reviews/reviews.java
+++ Application/src/main/java/Restaurants/randomizer/Reviews/reviews.java
@@ -1,4 +1,4 @@
-package model;
+package Restaurants.randomizer.Reviews;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
--- Application/src/main/java/Restaurants/randomizer/restuarant/restaurant.java
+++ Application/src/main/java/Restaurants/randomizer/restuarant/restaurant.java
@@ -1,10 +1,10 @@
-package model;
+package Restaurants.randomizer.restuarant;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
-@DynamoDBTable(tableName = "Restaurants")
+@DynamoDBTable(tableName = "Restaurants/randomizer")
 public class restaurant {
     String id;
 
