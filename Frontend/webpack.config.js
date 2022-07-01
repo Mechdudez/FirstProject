ab7f875874a7854de42a9763705ffbefa11e2c05
jSydorowicz21 <jonathan.sydorowicz@mykenzie.snhu.edu>
2022-07-01 11:18:43 -0500

    Fixes

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -17,14 +17,39 @@ module.exports = {
   },
   devServer: {
     https: false,
+
     port: 8080,
+
     open: true,
+
     openPage: 'http://localhost:8080',
+
     // diableHostChecks, otherwise we get an error about headers and the page won't render
+
     disableHostCheck: true,
+
     contentBase: 'packaging_additional_published_artifacts',
+
     // overlay shows a full-screen overlay in the browser when there are compiler errors or warnings
-    overlay: true
+
+    overlay: true,
+
+    proxy: [
+
+      {
+
+        context: [
+
+          '/restaurant',
+          'review',
+
+        ],
+
+        target: 'http://localhost:5001'
+
+      }
+
+    ]},
   },
   plugins: [
     new HtmlWebpackPlugin({
