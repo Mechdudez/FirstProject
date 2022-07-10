4c6500511691a5a77384404cb31c7fb7b31a77cd
Ryan Wiest <james.wiest@mykenzie.snhu.edu>
2022-07-09 21:17:14 -0700

    Update SampleRestaurantGenerator.java

--- Application/src/main/java/com/kenzie/restaurant/randomizer/controller/sampleRestaurants/SampleRestaurantGenerator.java
+++ Application/src/main/java/com/kenzie/restaurant/randomizer/controller/sampleRestaurants/SampleRestaurantGenerator.java
@@ -5,14 +5,18 @@ import com.kenzie.restaurant.randomizer.service.model.Restaurant;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class SampleRestaurantGenerator {
 
     public static List<Restaurant> readData() throws IOException {
 
-       // TODO Make a restaurant object list.
-        // return list, so it doesn't go poof
+        List<Restaurant> restaurantList = new ArrayList<>(); // Will hold the restaurants, so they don't go poof
+        List<String[]> stringList = new ArrayList<>();
+
+
         try (BufferedReader br = Files.newBufferedReader(Paths.get("C:\\Users\\Owner\\ata-lbc-project-Kane-Ryan-Jonathan\\Application\\src\\main\\java\\com\\kenzie\\restaurant\\randomizer\\controller\\sampleRestaurants\\Restaurant.csv"))) {
 
             // CSV file delimiter
@@ -24,18 +28,41 @@ public class SampleRestaurantGenerator {
 
                 // convert line into columns
                 String[] columns = line.split(DELIMITER);
+                for (int i = 0; i < columns.length; i++) {
+                    columns[i] = columns[i].replaceAll("^\"|\"$", "");
 
+                }
                 // print all columns
-                System.out.println("Restaurant[" + String.join(", ", columns) + "]");
-            }
+                //  System.out.println("Restaurant[" + String.join(", ", columns) + "]");
+                // adds all the Strings into a List.
+                stringList.add(columns);
 
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
 
         }
-        return null;
-    }
-    public static void main (String[]args) throws IOException {
-readData();
+        // Have to get rid of the double quotes, by iterating through the list?
+
+
+// Have to iterate through the String array for each restaurant object
+        for (String[] detailsRestaurant : stringList) {
+            Restaurant restaurant = new Restaurant();
+            UUID uuid = UUID.randomUUID();
+            restaurant.setRestaurantId(uuid);
+            restaurant.setAveragePrice(Double.valueOf(detailsRestaurant[1]));
+            restaurant.setAverageRating(Double.valueOf(detailsRestaurant[2]));
+            restaurant.setCategory(detailsRestaurant[3]);
+            restaurant.setRestaurantName(detailsRestaurant[4]);
+
+            restaurantList.add(restaurant); // Only grabbing the first one. How can I get it to grab each one?
+
         }
+        System.out.println(restaurantList);
+        return restaurantList;
     }
+
+    public static void main(String[] args) throws IOException {
+        readData();
+    }
+}
