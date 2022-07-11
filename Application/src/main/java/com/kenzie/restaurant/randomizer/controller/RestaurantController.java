85779de58da559101c67595c069fa7e1ff9c5106
jSydorowicz21 <jonathan.sydorowicz@mykenzie.snhu.edu>
2022-07-11 11:46:05 -0500

    New pathing

--- Application/src/main/java/com/kenzie/restaurant/randomizer/controller/RestaurantController.java
+++ Application/src/main/java/com/kenzie/restaurant/randomizer/controller/RestaurantController.java
@@ -92,18 +92,6 @@ public class RestaurantController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/within/{price}{category}")
-    public ResponseEntity<RestaurantResponse> getSortedRestaurant(@PathVariable("price") Double price, @PathVariable("category") String category) {
-        Restaurant restaurant = restaurantService.getSortedRestaurant(price, category);
-
-        if (restaurant == null) {
-            return ResponseEntity.noContent().build();
-        }
-
-        RestaurantResponse response = createRestaurantResponse(restaurant);
-
-        return ResponseEntity.ok(response);
-    }
 
     @GetMapping("/random")
     public ResponseEntity<RestaurantResponse> getRandomRestaurant() {
